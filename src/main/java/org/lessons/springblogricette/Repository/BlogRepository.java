57922fa80663356f7bf072733056f44ad47a5e18
AlfredoM06 <Alfredomanfredi06@gmail.com>
2024-01-22 10:24:40 +0100

    creato controller, model, repo e creato i templates

--- src/main/java/org/lessons/springblogricette/Repository/BlogRepository.java
+++ src/main/java/org/lessons/springblogricette/Repository/BlogRepository.java
@@ -1,6 +1,7 @@
 package org.lessons.springblogricette.Repository;
 
+import org.lessons.springblogricette.Model.Ricetta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BlogRepository extends JpaRepository {
+public interface BlogRepository extends JpaRepository<Ricetta, Integer> {
 }
