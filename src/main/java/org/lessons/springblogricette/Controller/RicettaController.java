0609c31ef26bccb3f30cf022f69147a063b39b20
AlfredoM06 <Alfredomanfredi06@gmail.com>
2024-01-22 11:27:31 +0100

    creati file html per le crud e fatto il controller per i dettagli

--- src/main/java/org/lessons/springblogricette/Controller/RicettaController.java
+++ src/main/java/org/lessons/springblogricette/Controller/RicettaController.java
@@ -3,12 +3,16 @@ package org.lessons.springblogricette.Controller;
 import org.lessons.springblogricette.Model.Ricetta;
 import org.lessons.springblogricette.Repository.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/ricette")
@@ -25,5 +29,17 @@ public class RicettaController {
         return "ricette/list";
     }
 
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Ricetta> result = blogRepository.findById(id);
+        if (result.isPresent()) {
+            Ricetta ricetta = result.get();
+            model.addAttribute("ricetta", ricetta);
+            return "ricette/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found");
+
+        }
+    }
 
 }
